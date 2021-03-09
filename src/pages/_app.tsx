9b4c4e08d1f88ab800c1f4287d01ff944f3813df
Iqbal Fasri <iqbalx64@gmail.com>
2021-03-10 00:53:14 +0700

    add google tag manager

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,25 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
+
 import "../styles/font.css";
 import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouterChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouterChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 
